212869ddcf6f5665802e63c5ab28f9703c0742c0
lingraj1124 <82047703+lingraj1124@users.noreply.github.com>
2023-02-22 22:49:22 +0530

    first commit
    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <p>Website under Maintenance</p>
+      </div> */}
+      <div class="overlay"></div>
+<div class="stars" aria-hidden="true"></div>
+<div class="starts2" aria-hidden="true"></div>
+<div class="stars3" aria-hidden="true"></div>
+<main class="main">
+	<section class="contact">
+		<h2 class="sub-title">Site Under Construction</h2>
+	</section>
+</main>
     </div>
   );
 }
